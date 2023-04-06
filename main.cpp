617d10eb6928807a3185bc8097b4cc90ea52f73a
Jakub Dębosz <56394099+Doseeey@users.noreply.github.com>
2023-04-06 18:25:27 +0200

    Init project w/ csv data

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+#define MAX_SIZE 1010295
+
+std::string get_csv_data(std::string fileName) {
+    std::string data[MAX_SIZE];
+
+    std::fstream file(fileName, std::ios::in);
+    // if(file.is_open()) {
+
+    // }
+}
 
 int main() {
     return 0;
